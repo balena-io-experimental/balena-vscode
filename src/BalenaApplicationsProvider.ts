d2b962556cf8ef1f950ffa7944d92d3ddca6fa2b
Trevor Sullivan <trevor@balena.io>
2019-01-14 07:42:17 -0800

    Cleaning up commented code from VSCode extension template

--- src/BalenaApplicationsProvider.ts
+++ src/BalenaApplicationsProvider.ts
@@ -46,32 +46,3 @@ export class BalenaApplicationsProvider implements vscode.TreeDataProvider<Balen
         });
     }
 }
-
-
-// export class Dependency extends vscode.TreeItem {
-
-// 	constructor(
-// 		public readonly label: string,
-// 		private version: string,
-// 		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-// 		public readonly command?: vscode.Command
-// 	) {
-// 		super(label, collapsibleState);
-// 	}
-
-// 	get tooltip(): string {
-// 		return `${this.label}-${this.version}`;
-// 	}
-
-// 	get description(): string {
-// 		return this.version;
-// 	}
-
-// 	iconPath = {
-// 		light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
-// 		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
-// 	};
-
-// 	contextValue = 'dependency';
-
-// }
