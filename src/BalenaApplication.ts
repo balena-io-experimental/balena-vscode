180fbb0bc8dfcc10437ba2c70ea9c66c0c78765b
Trevor Sullivan <trevor@balena.io>
2019-01-14 07:41:31 -0800

    Adding static helper method to retrieve list of balenaCloud applications as a string

--- src/BalenaApplication.ts
+++ src/BalenaApplication.ts
@@ -1,4 +1,6 @@
 import * as vscode from 'vscode';
+import fetch from 'node-fetch';
+import * as fs from 'fs';
 
 export class BalenaApplication extends vscode.TreeItem {
     constructor(
@@ -25,4 +27,25 @@ export class BalenaApplication extends vscode.TreeItem {
     }
 
     contextValue = 'balenaApplication';
+
+    // Helper function to retrieve a list of balenaCloud Application names as a string array
+    static getBalenaApplicationList(): Promise<string[]> {
+        let balenaToken = fs.readFileSync(`${process.env['USERPROFILE']}\\.balenaToken`);
+        return fetch('https://api.balena-cloud.com/v4/application', {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${balenaToken}`
+            }
+        })
+        .then(response => {
+            console.log(`Returning Balena API JSON ...`);
+            return response.json();
+        })
+        .then(payload => {
+            //console.log(payload.d);
+            return payload.d.map(item => {
+                return item.app_name;
+            });
+        });
+    }
 }
