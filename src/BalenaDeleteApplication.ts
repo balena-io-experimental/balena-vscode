6a52c01834cb3334b798347ab161b9db2470eb6b
Trevor Sullivan <trevor@balena.io>
2019-01-17 18:43:25 -0800

    Implement tested balenaCloud.DeleteApplication code Tested with one and multiple applications

--- src/BalenaDeleteApplication.ts
+++ src/BalenaDeleteApplication.ts
@@ -1,12 +1,31 @@
 import * as vscode from 'vscode';
 import { BalenaApplication } from './models/BalenaApplication';
+import fetch from 'node-fetch';
+import { getBalenaToken } from './BalenaHelpers';
 //import * as balena from 'balena-sdk';
 
-export function BalenaDeleteApplication() {
-    let disposable = vscode.commands.registerCommand('balenaCloud.DeleteApplication', () => {
-        console.log(`Registered balenaCloud.DeleteApplication`);
-        vscode.window.showQuickPick(BalenaApplication.getBalenaApplicationList(), { canPickMany: true });
-        
+export function BalenaDeleteApplication(): vscode.Disposable {
+    let disposable = vscode.commands.registerCommand('balenaCloud.DeleteApplication', async () => {
+        let itemList = await BalenaApplication.getBalenaApplicationQuickPick();
+        let appList = await vscode.window.showQuickPick(itemList, { canPickMany: true });
+        vscode.window.showWarningMessage(`Deleting application ${appList}`);
+        vscode.window.showWarningMessage(typeof appList);
+
+        // If user doesn't select any items, return nothing
+        if (!appList) { console.log(`User didn't select any balenaCloud apps to delete`); return; }
+
+        await appList.map(async app => {
+            // @ts-ignore
+            console.log(`Deleting ${app.label}`)
+            // @ts-ignore
+            await fetch(`https://api.balena-cloud.com/v4/application(${app.label})`, {
+                method: 'DELETE',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${getBalenaToken()}`,
+                }
+            });
+        });
     });
     return disposable;
 }
