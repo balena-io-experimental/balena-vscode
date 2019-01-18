d976d20c6c164c53d05e855a64d089994155dee4
Trevor Sullivan <trevor@balena.io>
2019-01-17 18:43:02 -0800

    Moved code to get BalenaApplications into model

--- src/BalenaApplicationsProvider.ts
+++ src/BalenaApplicationsProvider.ts
@@ -1,8 +1,7 @@
 import * as vscode from 'vscode';
 import fetch from 'node-fetch';
-import * as fs from 'fs';
 import { BalenaApplication } from './models/BalenaApplication';
-
+import { getBalenaToken } from './BalenaHelpers';
 export class BalenaApplicationsProvider implements vscode.TreeDataProvider<BalenaApplication> {
 
 	private _onDidChangeTreeData: vscode.EventEmitter<BalenaApplication | undefined> = new vscode.EventEmitter<BalenaApplication | undefined>();
@@ -19,27 +18,11 @@ export class BalenaApplicationsProvider implements vscode.TreeDataProvider<Balen
 	getChildren(element?: BalenaApplication): Thenable<BalenaApplication[]> {
         if (!element) {
             console.log(`Retrieving Balena Applications`);
-            return this.getBalenaApplications();
+            return BalenaApplication.getBalenaApplicationObjects();
         }
         else {
             console.warn(`Should not specify an element to get children of.`);
         }
     }
     
-    private getBalenaApplications(): Promise<BalenaApplication[]> {
-        let balenaToken = fs.readFileSync(`${process.env['USERPROFILE']}\\.balenaToken`);
-        return fetch('https://api.balena-cloud.com/v4/application', {
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${balenaToken}`
-            }
-        })
-        .then(response => {
-            console.log(`Returning Balena API JSON ...`);
-            return response.json();
-        })
-        .then(payload => {
-            return payload.d.map(item => new BalenaApplication(item.id, item.id, item.app_name));
-        });
-    }
 }
